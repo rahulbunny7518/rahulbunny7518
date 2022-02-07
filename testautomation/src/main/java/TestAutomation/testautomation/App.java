a7d3e24b4e28624cfe4a28fab6ff72bdb1a5f210
Rahul Narendhula <rahul_narendhula@epam.com>
2022-02-07 15:02:15 +0530

    project first commit

---
+++ testautomation/src/main/java/TestAutomation/testautomation/App.java
@@ -0,0 +1,13 @@
+package TestAutomation.testautomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
