1333dc6208f828bfaf187c4cd3b71a8cb370b3f2
Rahul Narendhula <rahul_narendhula@epam.com>
2022-03-08 22:01:52 +0530

    "RestAPi Project commit"

--- RestApiTestingProject/src/test/java/TestAutomation/RestApiTestingProject/PunchInOut.java
+++ RestApiTestingProject/src/test/java/TestAutomation/RestApiTestingProject/PunchInOut.java
@@ -24,7 +24,6 @@ public class PunchInOut {
 	}
 
 
-
 	public int getEmpNumber() {
 		return empNumber;
 	}
--- RestApiTestingProject/src/test/java/TestAutomation/RestApiTestingProject/TestOrangeHRM.java
+++ RestApiTestingProject/src/test/java/TestAutomation/RestApiTestingProject/TestOrangeHRM.java
@@ -183,4 +183,5 @@ public class TestOrangeHRM {
 		assertEquals(res,"Successfully Deleted");
 		}
 	
+	
 }
